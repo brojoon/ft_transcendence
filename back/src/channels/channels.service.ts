5a41d34d5e6d02b74ccdc65895279737a937a1fc
20151883 <nyz1058@nate.com>
2021-11-02 13:09:01 +0900

    channel service update

--- back/src/channels/channels.service.ts
+++ back/src/channels/channels.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Chatchannel } from 'src/entities/Chatchannel';
 import { Chatcontent } from 'src/entities/Chatcontent';
 import { Chatmember } from 'src/entities/Chatmember';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
-
+import * as bcrypt from "bcrypt";
+import { NotFoundError } from 'rxjs';
 @Injectable()
 export class ChannelsService {
   constructor(
@@ -15,78 +16,136 @@ export class ChannelsService {
     @InjectRepository(Chatcontent) private chatcontentRepository: Repository<Chatcontent>,
   ) { }
 
+  private salt:number;
+
   // private 제외
   async allChannelList() {
+    const ret = await this.chatchannelRepository.find({select : ["id", "name", "type", "createdAt", "deleteAt", "updatedAt"]});
 
+    return ret;
   }
 
-  async myChannelList() {
+  async myChannelList(userid:string) {
+    const ret = await this.chatchannelRepository.createQueryBuilder('c')
+    .leftJoin('c.Chatmembers', 'm')
+    .where('m.userId = :userid', {userid})
+    .select (["c.id", "c.name", "c.type", "c.createdAt", "c.deleteAt", "c.updatedAt"])
+    .getMany();
 
+    return ret;
   }
 
-  async makeChannel() {
+  async makeChannel(userId:string, channelName:string, channelType:number, password:string = null) {
+    //const obj:Chatchannel = {};
+    const newChannel = new Chatchannel();
+    newChannel.name = channelName;
+    newChannel.type = channelType;
+    if (!password)
+    {
+      const newPassword = bcrypt.hash(password, 12);
+      newPassword.
+      newChannel.password = newPassword;
+    }
+    const channel = await this.chatchannelRepository.save(newChannel);
 
+    const newMember = new Chatmember();
+    newMember.userId = userId;
+    newMember.channelId = channel.id;
+    newMember.auth = 2;
+    const member = await this.chatmemberRepository.save(newMember);
   }
 
-  async joinChannel() {
-
-  }
-
-  async getOutChnnel() {
-
-  }
-
-  async inviteChnnel() {
-
-  }
-
-  async snedMessage() {
-
-  }
-
-  async getAllMessage() {
-
-  }
-
-  async userList() {
-
-  }
-
-  async checkMuteState() {
-
-  }
-
-  async checkAdmin() {
-
-  }
-
-  async giveAdmin() {
+  async joinChannel(channelId:number, userId:string, channelPassword:any = null) {
+    const channel = await this.chatchannelRepository.findOne({where : {id:channelId}});
+    if (channel.type == 1)
+    {
+      const test = bcrypt.compare(channelPassword, channel.password);//channelPassword = null일때?
+      if (!test)
+        throw new UnauthorizedException("비밀번호가 틀립니다");
+    }
     
+    const newMember = new Chatmember();
+    newMember.userId = userId;
+    newMember.channelId = channelId;
+    await this.chatmemberRepository.save(newMember);
   }
 
-  async banUser() {
-    
+  async getOutChnnel(channelId:number, userId:string) {
+    await this.chatmemberRepository.delete({channelId, userId});
+  }
+
+  async inviteChnnel(channelId:number, administerId:string, visitorId:string) {
+      const administer = await this.chatmemberRepository.findOne({where : {channelId, userId : administerId}});
+      if (administer.auth < 1)
+        throw new UnauthorizedException("administer가 아닌데 초대를 시도함.");
+      const newMember = new Chatmember();
+      newMember.userId = visitorId;
+      newMember.channelId = channelId;
+      await this.chatmemberRepository.save(newMember);
+  }
+
+  async sendMessage(channelId:number, sender:string, msg:string) {
+    const chatContent = new Chatcontent();
+    chatContent.userId = sender;
+    chatContent.channelId = channelId;
+    chatContent.message = msg;
+    await this.chatcontentRepository.save(chatContent);
+  }
+
+  async getAllMessage(channelId:number) {
+    const chatContent = await this.chatcontentRepository.find({where:{channelId}, select:["userId", "message", "updatedAt"]})
+    return chatContent;
+  }
+
+  async userList(channelId:number) {
+    const userList = await this.chatchannelRepository.createQueryBuilder('c')
+    .leftJoin('c.Chatmembers', 'm')
+    .where('c.id = :channelId', {channelId : channelId})
+    .select(["userId"])
+    .getMany();
+
+    return userList;
+  }
+
+  async checkMuteState(channelId:number, userId:string) :Promise<boolean>{
+    const chatmember = await this.chatmemberRepository.findOne({where:{channelId, userId}});
+    return chatmember.mute;
+  }
+
+  async checkAdmin(channelId:number, userId:string) {
+    const chatmember = await this.chatmemberRepository.findOne({where:{channelId, userId}});
+    return chatmember.auth;//number로 반환?
+    return (chatmember.auth > 1);//bool로 반환? 
+  }
+
+  async giveAdmin(channelId:number, userId:string) {
+    await this.chatmemberRepository.update({channelId, userId}, {auth:1})
+  }
+
+  async banUser(channelId:number, userId:string) {//??
+    await this.chatmemberRepository.update({channelId, userId}, {mute:true})
   }
 
   // 0 = publice,  1 = protected, 2 = private
-  async updateType() {
-
+  async updateType(channelId:number, type:number) {
+    await this.chatchannelRepository.update({id:channelId}, {type})
   }
 
-  async updateChannelName() {
-
+  async updateChannelName(channelId:number, name:string) {
+    await this.chatchannelRepository.update({id:channelId}, {name})
   }
 
-  async updateChannelPassword() {
-
+  async updateChannelPassword(channelId:number, password:string) {
+    const newPassword = bcrypt.hash(password, 12);
+    await this.chatchannelRepository.update({id:channelId}, {password : newPassword});
   }
 
-  async muteSwitch() {
-
+  async muteSwitch(channelId, userId) {
+    await this.chatmemberRepository.update({channelId, userId}, {mute:!`{mute}`})
   }
 
-  async deleteChannel() {
-
+  async deleteChannel(channelId:number) {
+    await this.chatchannelRepository.delete({id:channelId});
   }
 
 }
