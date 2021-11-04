4fc7809866d6a82164eb4208f6853c394733cb5c
oddczv1 <oddczv123@gmail.com>
2021-11-04 18:09:27 +0900

    간다한 것들

--- back/src/game/gameInit.ts
+++ back/src/game/gameInit.ts
@@ -2,13 +2,36 @@ const initData ={
     interval: null,
     player_one_point: 0,
     player_two_point: 0,
+    player_one_state: 0,
+    player_two_state: 0,
+    game_state: 0,
+    game_map: 0,
+    game_set: 3,
+    complete_game_set: 0,
+    bar_seed: 30,
     ball_x: 500,
     ball_y: 250,
-    ball_move_x: 2,
-    ball_move_y: 2,
     player_one_y: 200,
     player_two_y: 200,
-    bar_seed:30
 }
-
 export = initData;
+
+//// player_one_state / player_two_state ////
+// 0 -> 비접속
+// 1 -> 접속
+// 2 -> 레디
+
+//// game_state ////
+// 0 -> 대기
+// 1 -> 경기중
+// 2 -> 일시중지
+// 3 -> 게임 완료
+
+//// game_map ////
+// 0 -> 기본 맵
+// 1 -> 1번 맵
+// 2 -> 2번 맵
+// 3 -> 3번 맵
+
+//// game_set ////
+// 총 경기수
