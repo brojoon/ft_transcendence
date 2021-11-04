56b96eb164567aa6c6fbf23f0521d2b06dd24a45
oddczv1 <oddczv123@gmail.com>
2021-11-04 18:16:41 +0900

    api/game

--- back/src/game/game.controller.ts
+++ back/src/game/game.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get} from '@nestjs/common';
 import { GameService } from './game.service';
 
-@Controller('game')
+@Controller('api/game')
 export class GameController {
   constructor(
     private readonly gameService: GameService
