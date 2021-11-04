720d11330cb7fe1e012a56289081677b88e927ef
20151883 <nyz1058@nate.com>
2021-11-04 18:05:55 +0900

    game update 4

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -26,19 +26,31 @@ export class GameService {
     gameMap[gameId].ball_y += gameMap[gameId].dir_y * gameMap[gameId].length;
     //console.log("ball position", gameMap[gameId].ball_x, gameMap[gameId].ball_y);
     this.emit(gameId);
-    if (gameMap[gameId].ball_y + 5 > 500 || gameMap[gameId].ball_y - 5 < 0) {
+    if (gameMap[gameId].ball_y + 5 >= 500 || gameMap[gameId].ball_y - 5 <= 0) {
       //gameMap[gameId].ball_move_y *= -1;
       const test = this.chageDir(gameMap[gameId].dir_x, gameMap[gameId].dir_y, 1, 0);
       gameMap[gameId].dir_x = test.newDir_x;
       gameMap[gameId].dir_y = test.newDir_y;
       console.log("changeDir", gameMap[gameId].dir_x, gameMap[gameId].dir_y);
     }
-    if (gameMap[gameId].ball_x >= 980 || gameMap[gameId].ball_x <= 20) {
+    else if (gameMap[gameId].ball_x >= 980 || gameMap[gameId].ball_x <= 20) {
       if (this.isPannel(gameId) === true){
+        
         console.log("changeDir2", gameMap[gameId].dir_x, gameMap[gameId].dir_y);
         const test = this.chageDir(gameMap[gameId].dir_x, gameMap[gameId].dir_y, 0, 1);
         gameMap[gameId].dir_x = test.newDir_x;
         gameMap[gameId].dir_y = test.newDir_y;
+        let wantRandomPannel = true;
+        if (wantRandomPannel){
+          let rand = Math.random();
+          rand *= 10000;
+          rand += 1;
+          rand = Math.round(rand);
+          rand %= 7;
+          const x_sign = gameMap[gameId].dir_x > 0 ? +1 : -1;
+          gameMap[gameId].dir_x = x_sign * Math.abs(Math.cos(rand));
+          gameMap[gameId].dir_y = Math.sin(rand);
+        }
       }
       else{
         clearInterval(gameMap[gameId].interval);
@@ -54,7 +66,17 @@ export class GameService {
     gameMap[gameId].ball_y = 250;
     gameMap[gameId].dir_x = 1.0 / Math.sqrt(2);
     gameMap[gameId].dir_y = 1.0 / Math.sqrt(2);
-    gameMap[gameId].length = 1;
+    gameMap[gameId].dir_x = Math.sqrt(3) / 2.0;
+    gameMap[gameId].dir_y = 1 / 2;
+    let rand = Math.random();
+    rand *= 10000;
+    rand += 1;
+    rand = Math.round(rand);
+    rand %= 7;
+    gameMap[gameId].dir_x = Math.cos(rand);
+    gameMap[gameId].dir_y = Math.sin(rand);
+
+    gameMap[gameId].length = 2;
     //gameMap[gameId].ball_move_x = 2;
     //gameMap[gameId].ball_move_y = 2;
     gameMap[gameId].player_one_y = 200;
@@ -81,18 +103,31 @@ export class GameService {
   chageDir(dir_x:number, dir_y:number, vertical_x, vertical_y){//dir_x, dir_y 방향벡터의 크기는 1, vertical_x y 둘중 하나는 0 나머지 하나는 1;
     
     //내적해서 cos(@)를 구함
-    const cosA = Math.abs((vertical_x * dir_x) + (vertical_y * dir_y));//abs해서 작은 @를 구함.
-    const sinA = Math.sqrt(1 - Math.pow(cosA, 2));
+    let cosA = Math.abs((vertical_x * dir_x) + (vertical_y * dir_y));//abs해서 작은 @를 구함.
+    const theta = Math.acos(cosA);
+    //const sinA = Math.sqrt(1 - Math.pow(cosA, 2));
+    let sinA = Math.sin(theta);
+    console.log("before cosA", cosA);
+    cosA = Math.cos(theta);
+    console.log("after cosA", cosA);
     //회전각도(= 파이-2@)를 구함
-    const cosR = -1 * (Math.pow(cosA, 2) - Math.pow(sinA, 2));
-    const sinR = 2 * cosA * sinA;
+    //const cosR = -1 * (Math.pow(cosA, 2) - Math.pow(sinA, 2));
+    //const sinR = 2 * cosA * sinA;
+    let cosR = -1 * Math.cos(2 *theta)
+    console.log("before cosR", cosR);
+    cosR = Math.cos(Math.PI - 2 * theta);
+    console.log("after cosR", cosR);
+    const sinR = Math.sin(2 * theta);
+    
     //벡터회전을 진행함
-    let sign1 = dir_x * dir_y > 0 ? -1 : +1;//곱이 양수면 시계 회전, 음수이면 반시계회전
+    let sign1 = dir_x * dir_y > 0 ? +1 : -1;//곱이 양수면 시계 회전, 음수이면 반시계회전
     let sign2 = vertical_x + -1 * vertical_y;//왼,오른쪽에 부딪힌다면 회전방향이 반대로 바꿔줘야함
     //시계회전이 "+부호 회전", 반시계회전이 "-부호 회전"
     const sign = sign1 * sign2;
-    const newDir_x = cosR * dir_x - (sign * sinR * dir_y);
-    const newDir_y = (sign * sinR * dir_x) + (cosR * dir_y);
+    const t = sign > 0 ? "양수" : "음수";
+    console.log(t, "방향으로 회전합니다");
+    const newDir_x = cosR * -1 *dir_x - (sign * sinR * -1 * dir_y);
+    const newDir_y = (sign * sinR * -1 *dir_x) + (cosR * -1 * dir_y);
     return {newDir_x, newDir_y};//방향벡터의 크기가 1이 아닌경우가 존재할수 있는지..?
   }
 }
