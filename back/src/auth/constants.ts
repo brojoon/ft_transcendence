cd7c6d9624060c0fc9a99a6b59409cd4c9bf4b71
20151883 <nyz1058@nate.com>
2022-01-05 22:26:16 +0900

    ip주소값 수정

--- back/src/auth/constants.ts
+++ back/src/auth/constants.ts
@@ -5,16 +5,16 @@ export const jwtConstants = {
 
   CLIENT_ID: '37f1a6e531b74604cf47425476d8681b886f1dd638e9cb1a673131cffbc84256',
   CLIENT_SECRET: '65039aaaff292c107f3941cdedbf474f30305b4e055728c59d821ccffc545930',
-  CALLBACK_URL: 'http://34.82.79.134:8081/api/auth/42/callback',
+  CALLBACK_URL: 'http://193.123.238.90:8081/api/auth/42/callback',
 
   KAKAO_CLIENT_ID: 'd95ea8012bf88a97af2828e546712f74',
   KAKAO_SECRET: '',
-  KAKAO_CALLBACK_URL: 'http://34.82.79.134:8081/api/auth/kakao/redirect',
+  KAKAO_CALLBACK_URL: 'http://193.123.238.90:8081/api/auth/kakao/redirect',
 
   GOOGLE_CLIENT_ID: '620069586851-q6upth2mhck23ggjrk023monqih7dqph.apps.googleusercontent.com',
   GOOGLE_SECRET: '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',
-  GOOGLE_CALLBACK_URL: 'http://api.42transcendence.ml:8081/api/auth/google/redirect',
+  GOOGLE_CALLBACK_URL: 'http://193.123.238.90:8081/api/auth/google/redirect',
 
-  HOME: 'http://34.82.79.134:8082/home',
-  TWO_FACTOR: 'http://34.82.79.134:8082/two-factor'
+  HOME: 'http://193.123.238.90:8082/home',
+  TWO_FACTOR: 'http://193.123.238.90:8082/two-factor'
 };
--- front/utils/useSocket.ts
+++ front/utils/useSocket.ts
@@ -6,7 +6,7 @@ let socket: any = undefined;
 const getSocket = (() => {
 
 	if (!socket) {
-		socket = io.connect('http://34.82.79.134:8081');
+		socket = io.connect('http://193.123.238.90:8081');
 	}
 
 	return socket;
