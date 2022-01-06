bfa6b49425ec8ba2670d062fd001e90e6f541f2c
BroJoon <ydngjink1234@gmail.com>
2022-01-07 06:27:44 +0900

    fix: gameMapCheck

--- back/src/events/events.gateway.ts
+++ back/src/events/events.gateway.ts
@@ -34,7 +34,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   @SubscribeMessage('login')
   async handleLogin(
     @MessageBody() data: { userId: string; username: string, Dms: number[], channels: number[] },
-    @ConnectedSocket() socket: Socket ){
+    @ConnectedSocket() socket: Socket) {
 
     onlineMap[socket.id] = { userId: data.userId, username: data.username };
     console.log(`login : ${socket.id}, ${onlineMap[socket.id].userId}`);
@@ -78,10 +78,10 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
       if (onGameMap_gameId[gameId] !== undefined) {
         // player 나갈 때마다 하나씩 제거
         onGameMap_gameId[gameId].forEach(function (item, index, arr) {
-          if (item === onlineMap[socket.id].userId){
+          if (item === onlineMap[socket.id].userId) {
             onGameMap_gameId[gameId].splice(index, 1);
             if (onGameMap_gameId[gameId].length === 0) {
-              delete onGameMap_gameId[gameId]; 
+              delete onGameMap_gameId[gameId];
             }
           }
         });
@@ -89,39 +89,39 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
       // player1, player2 둘다 없으면 history state 2로만들고 dm에 상태도 2로 만듬
       // 그리고 dm에 신호주기?
-      let history = await this.historyRepository.findOne({id: gameId});
+      let history = await this.historyRepository.findOne({ id: gameId });
       if (history) {
         if (history.state === 1 && onGameMap_gameId[gameId] !== undefined) {
           let num = 0;
           onGameMap_gameId[gameId].forEach(function (item, index, arr) {
-            if (item === history.userId1 || item === history.userId2){
+            if (item === history.userId1 || item === history.userId2) {
               num++;
             }
           });
           if (num === 0) {
-            await this.historyRepository.update({id: gameId}, {state: 2})
-            await this.dmcontentRepository.update({historyId: gameId}, {match: 2})
+            await this.historyRepository.update({ id: gameId }, { state: 2 })
+            await this.dmcontentRepository.update({ historyId: gameId }, { match: 2 })
             // let dm = await this.dmcontentRepository.findOne({historyId: gameId});
             // this.server.to(`dm-${dm.dmId}`).emit('dm', null); 
           }
         } else if (history.state === 1 && onGameMap_gameId[gameId] === undefined) {
-          await this.historyRepository.update({id: gameId}, {state: 2})
-          await this.dmcontentRepository.update({historyId: gameId}, {match: 2})        
+          await this.historyRepository.update({ id: gameId }, { state: 2 })
+          await this.dmcontentRepository.update({ historyId: gameId }, { match: 2 })
         }
       }
       delete onlineMap[socket.id];
       // 접속상태 업데이트
       socket.to("all").emit('onlineList', Object.values(onlineMap));
-    } 
+    }
   }
 
   //////////////////////////////////////////////////////////////////////////
   //////////////////////////game 관련////////////////////////////////////////
   @SubscribeMessage('game')
   async gamejoin(
-    @MessageBody() data: {gameId: number, player: string, player1Ready: number, player2Ready: number},
-    @ConnectedSocket() socket: Socket ){
-    if (data.player !=="" && gameMap[data.gameId] === undefined) {
+    @MessageBody() data: { gameId: number, player: string, player1Ready: number, player2Ready: number },
+    @ConnectedSocket() socket: Socket) {
+    if (data.player !== "" && gameMap[data.gameId] === undefined) {
       // gameMap[data.gameId] = initData;
       gameMap[data.gameId] = {
         interval: null,
@@ -130,9 +130,9 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         player_one_ready: 0,
         player_two_ready: 0,
         game_map: 0,
-        game_count:0,
+        game_count: 0,
         game_set: 5,
-        random_map:0,
+        random_map: 0,
         game_state: 0,
         game_start: 0,
         complete_game_set: 0,
@@ -151,15 +151,15 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
       gameMap[data.gameId].player_two_ready = data.player2Ready;
       this.server.to(`game-${data.gameId}`).emit('ready', {
         player1: gameMap[data.gameId].player_one_ready,
-        player2: gameMap[data.gameId].player_two_ready, 
+        player2: gameMap[data.gameId].player_two_ready,
       });
     }
   }
 
   @SubscribeMessage('onGame')
   async onGame(
-    @MessageBody() data: {gameId: number, player: string},
-    @ConnectedSocket() socket: Socket ){
+    @MessageBody() data: { gameId: number, player: string },
+    @ConnectedSocket() socket: Socket) {
     if (onGameMap_gameId[data.gameId] === undefined) {
       let player_arr = [];
       onGameMap_gameId[data.gameId] = player_arr;
@@ -179,38 +179,38 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('offGame')
   async offGame(
-    @MessageBody() data: {gameId: number, player: string},
-    @ConnectedSocket() socket: Socket ){
+    @MessageBody() data: { gameId: number, player: string },
+    @ConnectedSocket() socket: Socket) {
     // onGameMap_gameId //
     if (onGameMap_gameId[data.gameId] !== undefined) {
       // player 나갈 때마다 하나씩 제거
       onGameMap_gameId[data.gameId].forEach(function (item, index, arr) {
-        if (item === data.player){
+        if (item === data.player) {
           onGameMap_gameId[data.gameId].splice(index, 1);
           if (onGameMap_gameId[data.gameId].length === 0) {
-            delete onGameMap_gameId[data.gameId]; 
+            delete onGameMap_gameId[data.gameId];
           }
         }
       });
     }
     // player1, player2 둘다 없으면 history state 2로만들고 dm에 상태도 2로 만듬
     // 그리고 dm에 신호주기?
-    let history = await this.historyRepository.findOne({id: data.gameId});
+    let history = await this.historyRepository.findOne({ id: data.gameId });
     if (history) {
       if (history.state === 1 && onGameMap_gameId[data.gameId] !== undefined) {
         let num = 0;
         onGameMap_gameId[data.gameId].forEach(function (item, index, arr) {
-          if (item === history.userId1 || item === history.userId2){
+          if (item === history.userId1 || item === history.userId2) {
             num++;
           }
         });
         if (num === 0) {
-          await this.historyRepository.update({id: data.gameId}, {state: 2})
-          await this.dmcontentRepository.update({historyId: data.gameId}, {match: 2})
+          await this.historyRepository.update({ id: data.gameId }, { state: 2 })
+          await this.dmcontentRepository.update({ historyId: data.gameId }, { match: 2 })
         }
       } else if (history.state === 1 && onGameMap_gameId[data.gameId] === undefined) {
-        await this.historyRepository.update({id: data.gameId}, {state: 2})
-        await this.dmcontentRepository.update({historyId: data.gameId}, {match: 2})        
+        await this.historyRepository.update({ id: data.gameId }, { state: 2 })
+        await this.dmcontentRepository.update({ historyId: data.gameId }, { match: 2 })
       }
     }
     // onGameMap //
@@ -223,29 +223,32 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('gameCheck')
   async gameCheck(
-    @MessageBody() data: {gameId: number},
-    @ConnectedSocket() socket: Socket){
+    @MessageBody() data: { gameId: number },
+    @ConnectedSocket() socket: Socket) {
     socket.join(`game-${data.gameId}`);
     if (gameMap[data.gameId] === undefined) {
       this.server.to(`game-${data.gameId}`).emit('gameStart', {
-        gameStart: 0       
+        gameStart: 0
       });
     }
     else {
       this.server.to(`game-${data.gameId}`).emit('gameStart', {
-        gameStart: gameMap[data.gameId].game_start       
+        gameStart: gameMap[data.gameId].game_start
       });
+      if (gameMap[data.gameId].game_start === 1) {
+        this.server.to(`game-${data.gameId}`).emit('gameMapCheck', { game_map: gameMap[data.gameId].game_map });
+      }
     }
   }
 
   @SubscribeMessage('matching')
   async gameMatch(
-    @MessageBody() data: {userId: string, gameId: number},
-    @ConnectedSocket() socket: Socket ){
+    @MessageBody() data: { userId: string, gameId: number },
+    @ConnectedSocket() socket: Socket) {
     socket.join('match');
 
-    if ((users.playerOne === null && data.gameId === 0) || 
-      (users.playerOne !== null && users.playerTwo !== null  && data.gameId === 0)) {
+    if ((users.playerOne === null && data.gameId === 0) ||
+      (users.playerOne !== null && users.playerTwo !== null && data.gameId === 0)) {
       users.playerOne = data.userId;
       users.playerOne_socet = socket;
       users.playerTwo = null;
@@ -259,13 +262,13 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         gameId: 0
       }
       this.server.to('match').emit('matched', matched)
-    } else if ( data.gameId !== 0){
+    } else if (data.gameId !== 0) {
       const matched = {
         playerOne: users.playerOne,
         playerTwo: users.playerTwo,
         gameId: data.gameId
       }
-      this.server.to('match').emit('matched', matched)      
+      this.server.to('match').emit('matched', matched)
       users.playerOne_socet.leave('match');
       users.playerTwo_socet.leave('match');
       users.playerOne = null;
@@ -277,9 +280,9 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('outMatching')
   async outMatching(
-    @MessageBody() data: {userId: string},
-    @ConnectedSocket() socket: Socket ){
-      
+    @MessageBody() data: { userId: string },
+    @ConnectedSocket() socket: Socket) {
+
     if (users.playerOne === data.userId && users.playerTwo === null) {
       users.playerOne = null;
       users.playerOne_socet = null;
@@ -288,19 +291,19 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   }
 
   @SubscribeMessage('gamePoint')
-  async gamePoint(@MessageBody() data: {gameId: number, user1Point: number, user2Point: number}){
+  async gamePoint(@MessageBody() data: { gameId: number, user1Point: number, user2Point: number }) {
     if (gameMap[data.gameId] !== undefined) {
       gameMap[data.gameId].player_one_point = data.user1Point;
       gameMap[data.gameId].player_two_point = data.user2Point;
       this.server.to(`game-${data.gameId}`).emit('point', {
         player1: data.user1Point,
-        player2: data.user2Point, 
+        player2: data.user2Point,
       });
     }
   }
 
   @SubscribeMessage('gameReady')  //userId 필요 없음
-  async gameReady(@MessageBody() data: {gameId: number, player: number, userId: string}){
+  async gameReady(@MessageBody() data: { gameId: number, player: number, userId: string }) {
     try {
       if (gameMap[data.gameId] !== undefined) {
         if (data.player === 1) {
@@ -310,8 +313,8 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         }
         this.server.to(`game-${data.gameId}`).emit('ready', {
           player1: gameMap[data.gameId].player_one_ready,
-          player2: gameMap[data.gameId].player_two_ready, 
-        }); 
+          player2: gameMap[data.gameId].player_two_ready,
+        });
       }
     } catch (error) {
       throw new BadRequestException("Ready 정보 업데이트 실패");
@@ -320,28 +323,28 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('changeGameSet')
   async changeGameSet(@MessageBody() data: {
-      gameId: number,
-      speed: number,
-      set: number,
-      map: number,
-      random: number,
-      check: string
-    }){
+    gameId: number,
+    speed: number,
+    set: number,
+    map: number,
+    random: number,
+    check: string
+  }) {
     try {
       if (gameMap[data.gameId] !== undefined) {
         if (data.check === undefined) {
           gameMap[data.gameId].length = data.speed;
           gameMap[data.gameId].game_set = data.set;
           gameMap[data.gameId].game_map = data.map;
-          gameMap[data.gameId].random_map = data.random;   
+          gameMap[data.gameId].random_map = data.random;
         }
         this.server.to(`game-${data.gameId}`).emit('gameSet', {
           length: gameMap[data.gameId].length,
-          game_set: gameMap[data.gameId].game_set, 
-          game_map: gameMap[data.gameId].game_map, 
+          game_set: gameMap[data.gameId].game_set,
+          game_map: gameMap[data.gameId].game_map,
           random_map: gameMap[data.gameId].random_map,
-        });  
-      }   
+        });
+      }
     } catch (error) {
       throw new BadRequestException("changeGameSet 실패")
     }
@@ -349,15 +352,15 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('player_one_up')
   async playerOneUP(@MessageBody() data: { game: number }) {
-    if (gameMap[data.game] !== undefined && gameMap[data.game].player_one_y > -50){
+    if (gameMap[data.game] !== undefined && gameMap[data.game].player_one_y > -50) {
       gameMap[data.game].player_one_y -= gameMap[data.game].bar_seed;
-      this.server.to(`game-${data.game}`).emit('player_one', { player_one_y: parseInt(gameMap[data.game].player_one_y)});
-    }  
+      this.server.to(`game-${data.game}`).emit('player_one', { player_one_y: parseInt(gameMap[data.game].player_one_y) });
+    }
   }
 
   @SubscribeMessage('player_one_down')
   async playerOneDown(@MessageBody() data: { game: number }) {
-    if (gameMap[data.game] !== undefined && gameMap[data.game].player_one_y < 450){
+    if (gameMap[data.game] !== undefined && gameMap[data.game].player_one_y < 450) {
       gameMap[data.game].player_one_y += gameMap[data.game].bar_seed;
       this.server.to(`game-${data.game}`).emit('player_one', { player_one_y: parseInt(gameMap[data.game].player_one_y) });
     }
@@ -365,15 +368,15 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
   @SubscribeMessage('player_two_up')
   async playerTwoUP(@MessageBody() data: { game: number }) {
-    if (gameMap[data.game] !== undefined && gameMap[data.game].player_two_y > -50){
+    if (gameMap[data.game] !== undefined && gameMap[data.game].player_two_y > -50) {
       gameMap[data.game].player_two_y -= gameMap[data.game].bar_seed;
       this.server.to(`game-${data.game}`).emit('player_two', { player_two_y: parseInt(gameMap[data.game].player_two_y) });
     }
   }
-  
+
   @SubscribeMessage('player_two_down')
   async playerTwoDown(@MessageBody() data: { game: number }) {
-    if (gameMap[data.game] !== undefined && gameMap[data.game].player_two_y < 450){
+    if (gameMap[data.game] !== undefined && gameMap[data.game].player_two_y < 450) {
       gameMap[data.game].player_two_y += gameMap[data.game].bar_seed;
       this.server.to(`game-${data.game}`).emit('player_two', { player_two_y: parseInt(gameMap[data.game].player_two_y) });
     }
