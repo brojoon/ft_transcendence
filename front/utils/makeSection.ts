f55e9835bb62bdce4a0279dccac5d7972cda6c1b
BroJoon <ydngjink1234@gmail.com>
2022-01-06 10:41:39 +0900

    fix: dayjs 시간보정

--- front/utils/makeSection.ts
+++ front/utils/makeSection.ts
@@ -1,5 +1,4 @@
 import { IChatList, IChannelChatList } from '@typings/db';
-import timezone from 'dayjs/plugin/timezone'
 import dayjs from 'dayjs';
 
 const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
@@ -7,7 +6,7 @@ const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '
 export function makeSectionDM(chatList: IChatList[]) {
 	const sections: { [key: string]: IChatList[] } = {};
 	chatList.forEach((chat) => {
-		let monthData = dayjs(chat.createdAt).format('YYYY-MM-DD');
+		let monthData = dayjs(chat.createdAt).subtract(9, "hour").format('YYYY-MM-DD');
 		monthData += ' ' + days[dayjs(chat.createdAt).day()];
 		if (Array.isArray(sections[monthData]))
 			sections[monthData].push(chat);
@@ -21,7 +20,7 @@ export function makeSectionChannel(chatList: IChannelChatList[]) {
 	const sections: { [key: string]: IChannelChatList[] } = {};
 	chatList.forEach((chat) => {
 		console.log(chat);
-		let monthData = dayjs(chat.updatedAt).format('YYYY-MM-DD');
+		let monthData = dayjs(chat.updatedAt).subtract(9, "hour").format('YYYY-MM-DD');
 		monthData += ' ' + days[dayjs(chat.updatedAt).day()];
 		console.log('monthData', monthData);
 		if (Array.isArray(sections[monthData]))
