8e16993bd38e85c92a0371ea95bfbe75f5e7bb69
brojoon <ydngjink1234@gmail.com>
2022-06-14 08:04:41 +0900

    chore: 알림 떄문에 삭제 했습니다.

--- back/src/auth/constants.ts
+++ back/src/auth/constants.ts
@@ -1,10 +1,3 @@
 export const jwtConstants = {
-  secret: 'secretKey',
-  PASSWORD: '42seoul',
-  AUTH_URL: 'https://api.intra.42.fr/oauth/authorize?client_id=7472a0931668c29a449afc031553e11079b9c1a154ce4910fa6775e0cb2517b3&redirect_uri=http%3A%2F%2Flocalhost%3A3095%2Fapi&response_type=code',
-  TOKEN_URL: 'https://api.intra.42.fr/oauth/token',
-  CLIENT_ID: '6ece2fe25646fc8fa7f15c67fe9e6fa466489d2cd32c3b293e77bcd7e98822c9',
-  CLIENT_SECRET: '681d5786cc86e3f8f9efaecb6cbcf050fcdbf9176d00a21e74cac6964bb3248c',
-  CALLBACK_URL: 'http://localhost:3095/auth/42/callback',
-  APP_NAME: '42seoul',
+
 };
